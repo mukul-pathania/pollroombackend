791224732c8027dcdb13c21ec58c5fa25134185b
Mukul Pathania <mukulpathania96@gmail.com>
2021-07-24 19:44:05 +0530

    Move the logic into controller functions
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

---
+++ app/controllers/auth.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import prisma from 'app/prismaClient';
+import passport from 'passport';
+
+export const SignUpWithEmailPassword = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password)
+      return res.json({
+        error: true,
+        message: 'Please provide all three of username, email and password',
+      });
+    const user = await prisma.user.findFirst({
+      where: { OR: [{ email: email }, { username: username }] },
+    });
+    if (user?.email === email)
+      return res.json({
+        error: true,
+        message: 'This email is already registered',
+      });
+    if (user?.username === username)
+      return res.json({
+        error: true,
+        message: 'This username is already taken',
+      });
+    const hash = await bcrypt.hash(password, 15);
+    await prisma.user.create({
+      data: {
+        email: email,
+        encrypted_password: hash,
+        username: username,
+        provider: 'EMAIL',
+      },
+    });
+    return res.json({ error: false, message: 'User signed up successfully' });
+  } catch (error) {
+    return res.json({
+      error: true,
+      message: 'An error occured while processing your request',
+    });
+  }
+};
+
+export const loginWithEmailPassword = (req: Request, res: Response) => {
+  passport.authenticate('local', function (err, user, message) {
+    if (err || !user) {
+      return res.json({ ...message, error: true });
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return res.json({ message: 'Failed to log you in', error: true });
+      }
+      return res.json({ ...message });
+    });
+  })(req, res);
+};
+
+export const logout = (req: Request, res: Response) => {
+  req.logOut();
+  res.json({ message: 'Logged out successfully' });
+};
+
+export const verify = (req: Request, res: Response) => {
+  if (req.user)
+    return res
+      .json({
+        message: 'You are authenticated',
+        isAuthenticated: true,
+        error: false,
+      })
+      .status(200);
+  else
+    return res.status(401).json({
+      message: 'You are not authenticated',
+      isAuthenticated: false,
+      error: false,
+    });
+};
+
+export const googleSignUpCallback = (req: Request, res: Response) => {
+  passport.authenticate('googleSignup', {}, (err, user, message) => {
+    return res.json({ ...message });
+  })(req, res);
+};
+
+export const googleLoginCallback = (req: Request, res: Response) => {
+  passport.authenticate('googleSignup', {}, (err, user, message) => {
+    return res.json({ ...message });
+  })(req, res);
+};
--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -1,84 +1,16 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import passport from 'passport';
-import prisma from '../prismaClient';
-import bcrypt from 'bcrypt';
+import * as authControllers from 'app/controllers/auth';
 
 const router = Router();
 
-router.post('/signup', async (req: Request, res: Response) => {
-  try {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password)
-      return res.json({
-        error: true,
-        message: 'Please provide all three of username, email and password',
-      });
-    const user = await prisma.user.findFirst({
-      where: { OR: [{ email: email }, { username: username }] },
-    });
-    if (user?.email === email)
-      return res.json({
-        error: true,
-        message: 'This email is already registered',
-      });
-    if (user?.username === username)
-      return res.json({
-        error: true,
-        message: 'This username is already taken',
-      });
-    const hash = await bcrypt.hash(password, 15);
-    await prisma.user.create({
-      data: {
-        email: email,
-        encrypted_password: hash,
-        username: username,
-        provider: 'EMAIL',
-      },
-    });
-    return res.json({ error: false, message: 'User signed up successfully' });
-  } catch (error) {
-    return res.json({
-      error: true,
-      message: 'An error occured while processing your request',
-    });
-  }
-});
+router.post('/signup', authControllers.SignUpWithEmailPassword);
 
-router.post('/login', (req: Request, res: Response) => {
-  passport.authenticate('local', function (err, user, message) {
-    if (err || !user) {
-      return res.json({ ...message, error: true });
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return res.json({ message: 'Failed to log you in', error: true });
-      }
-      return res.json({ ...message });
-    });
-  })(req, res);
-});
+router.post('/login', authControllers.loginWithEmailPassword);
 
-router.get('/logout', (req: Request, res: Response) => {
-  req.logOut();
-  res.json({ message: 'Logged out successfully' });
-});
+router.get('/logout', authControllers.logout);
 
-router.get('/verify', (req: Request, res: Response) => {
-  if (req.user)
-    return res
-      .json({
-        message: 'You are authenticated',
-        isAuthenticated: true,
-        error: false,
-      })
-      .status(200);
-  else
-    return res.status(401).json({
-      message: 'You are not authenticated',
-      isAuthenticated: false,
-      error: false,
-    });
-});
+router.get('/verify', authControllers.verify);
 
 router.get(
   '/google/signup',
@@ -87,11 +19,7 @@ router.get(
   }),
 );
 
-router.get('/google/signup/callback', (req: Request, res: Response) => {
-  passport.authenticate('googleSignup', {}, (err, user, message) => {
-    return res.json({ ...message });
-  })(req, res);
-});
+router.get('/google/signup/callback', authControllers.googleSignUpCallback);
 
 router.get(
   '/google/login',
@@ -100,32 +28,6 @@ router.get(
   }),
 );
 
-router.get('/google/login/callback', (req: Request, res: Response) => {
-  passport.authenticate('googleLogin', {}, (err, user, message) => {
-    if (err || !user) {
-      return res.json({ ...message });
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return res.json({ message: 'Failed to log you in' });
-      }
-      return res.json({ ...message });
-    });
-  })(req, res);
-});
-
-router.post('/login', (req: Request, res: Response) => {
-  passport.authenticate('local', function (err, user, message) {
-    if (err || !user) {
-      return res.json({ ...message });
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return res.json({ message: 'Failed to log you in' });
-      }
-      return res.json({ ...message });
-    });
-  })(req, res);
-});
+router.get('/google/login/callback', authControllers.googleLoginCallback);
 
 export default router;
