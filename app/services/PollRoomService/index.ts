43c90b4d0f7ac1142324fb3d3b2bc84bbbc395a9
Mukul Pathania <mukulpathania96@gmail.com>
2021-08-15 14:42:56 +0530

    Don't check for existance of room with same name, catch the error instead
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- app/services/PollRoomService/index.ts
+++ app/services/PollRoomService/index.ts
@@ -4,27 +4,30 @@ import prisma from '../../prismaClient';
 const createRoom = async (
   user: user,
   roomName: string,
-): Promise<{ message: string; error: boolean }> => {
+): Promise<{ message: string; error: boolean; roomId?: string }> => {
   try {
     const roomsCreatedByUser = await prisma.room.findMany({
       where: { creator_id: user.id },
+      select: { id: true },
     });
     if (roomsCreatedByUser.length >= 5)
       return {
         message: 'You have already created 5 rooms, delete some rooms first',
         error: true,
       };
-    const isNameTaken = await prisma.room.findFirst({
-      where: { name: roomName },
-    });
-    if (isNameTaken)
-      return { message: 'This name is already registered', error: true };
-    await prisma.room.create({
+    const room = await prisma.room.create({
       data: { creator_id: user.id, name: roomName },
     });
-    return { message: 'Success', error: false };
+    return { message: 'Success', error: false, roomId: room.id };
   } catch (error) {
     console.log(error);
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002')
+        return {
+          message: 'Room with this name is already registered',
+          error: true,
+        };
+    }
     return {
       error: true,
       message: 'An error occured while processing your request',
